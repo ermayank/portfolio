659fd693cd47370399d230fc38068ad135d1ecca
Mayank Gupta <mayank.pkgupta@outlook.com>
2021-03-05 23:30:13 +0530

    Contact page completed

--- js/appOther.js
+++ js/appOther.js
@@ -1,12 +1,8 @@
-const hamburger = document.querySelector(".hamburger");
 const hamburgerOther = document.querySelector(".hamburger-other");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links-ul li");
 
-hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("open");
 
-});
 hamburgerOther.addEventListener("click", () => {
     navLinks.classList.toggle("open");
 
